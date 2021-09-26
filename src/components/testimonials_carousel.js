86b7ad0ac7837cb2d633093aba7795abac470e2d
Bartek Rombalski <bartek.rombalski@gmail.com>
2021-09-26 10:10:14 +0200

    Fix: testimonials adjustments

--- src/components/testimonials_carousel.js
+++ src/components/testimonials_carousel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./testimonials_carousel.css";
 import { Carousel } from 'react-responsive-carousel';
 
 const getProps = () => ({
